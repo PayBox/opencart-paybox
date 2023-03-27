08d6f22db010df96fdfad050a7d7b2bf06e319e2
paybox <cms@paybox.money>
2023-03-27 08:53:41 +0000

    3.0.3

---
+++ catalog/language/en-gb/extension/payment/paybox.php
@@ -0,0 +1,7 @@
+<?php
+
+$_['text_title'] = 'Paybox (Visa, Mastercard)';
+$_['msg_description'] = 'Оплата по счету №%s';
+$_['err_order_not_found'] = 'Заказ не найден! Повторите попытку или свяжитесь с администрацией!';
+$_['button_confirm'] = 'Подтвердить заказ';
+$_['button_back'] = 'Вернуться';
